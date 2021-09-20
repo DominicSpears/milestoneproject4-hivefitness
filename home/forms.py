51510027def22abad4183daf806fdc7a38c3a4be
DominicSpears <dominicspears24@gmail.com>
2021-09-20 09:27:41 +0000

    Added contact form to home page.

---
+++ home/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    """
+    Contact Form on Landing Page
+    """
+    SUBJECT_CHOICES = (
+        ('Additional Equipment Recommendations',
+         'Additional Equipment Recommendations'),
+        ('Order Enquiries and Updates', 'Order Enquiries and Updates'),
+        ('Messageboard Queries', 'Messageboard Queries'),
+        ('Site Malfunction', 'Site Malfunction'),
+        ('Other', 'Other'),
+    )
+
+    name = forms.CharField(
+        label="Name"
+    )
+    email = forms.EmailField(
+        label="Email"
+    )
+    subject = forms.CharField(
+        label="Subject",
+        widget=forms.Select(choices=SUBJECT_CHOICES),
+    )
+    message = forms.CharField(
+        label="Message",
+        widget=forms.Textarea(attrs={
+            "rows": 7,
+        })
+    )
+
+    class Meta:
+        fields = ['name', 'email', 'subject', 'message']
--- home/views.py
+++ home/views.py
@@ -1,9 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from profiles.models import UserProfile
+
+from django.conf import settings
+
+from .forms import ContactForm
 
 # Create your views here.
 
 
 def index(request):
-    """ A view to return the index page """
+    """
+    Render index.html and ContactForm
+    """
+    template = 'home/index.html'
+    
+    contact_form = ContactForm()
 
-    return render(request, 'home/index.html')
+    context = {
+        'contact_form': contact_form,
+    }
+    return render(request, template, context)
+
+
+def contactform(request):
+    """
+    Send an email to the admin
+    when site visitors send message via contact form
+    """
+    contact_form = ContactForm(request.POST)
+    if contact_form.is_valid():
+        name = contact_form.cleaned_data['name']
+        email = contact_form.cleaned_data['email']
+        subject = contact_form.cleaned_data['subject']
+        message = contact_form.cleaned_data['message']
+        try:
+            send_mail(
+                f"You've got a message from {name} ({email}) on contact form.",
+                f"Subject: {subject}, Message: {message}",
+                email,
+                [settings.DEFAULT_FROM_EMAIL],
+            )
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+
+    return redirect('thankyou_page')
+
+
+def thankyou_page(request):
+    """
+    A view to render thank you page after site visitors send a contact form
+    """
+    return render(request, 'home/thankyou_page.html')
