b2fa1d57f3d8601f659c395ad5e222765e9f5744
DominicSpears <dominicspears24@gmail.com>
2021-09-20 10:11:43 +0000

    Add thank you page for contact form.

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('contactform', views.contactform, name='contactform'),
+    path('thankyou_page/', views.thankyou_page, name='thankyou_page'),
 ]
