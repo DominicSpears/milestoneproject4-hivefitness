2c490e73a31b70d3599bd2cd2c1c21d50b77dbad
DominicSpears <dominicspears24@gmail.com>
2021-09-21 18:33:10 +0000

    Add post detail template and basic layout.

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
+
 
 def all_posts(request):
     """ A view to show all blog posts """
@@ -13,3 +14,15 @@ def all_posts(request):
     }
 
     return render(request, 'posts/posts.html', context)
+
+
+def post_detail(request, post_id):
+    """ A view to show individual post details """
+
+    post = get_object_or_404(Post, pk=post_id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'posts/post_detail.html', context)
