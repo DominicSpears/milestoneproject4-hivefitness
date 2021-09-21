afda36f7504369a0c9cbb8df37147eb926091f34
DominicSpears <dominicspears24@gmail.com>
2021-09-21 17:10:20 +0000

    Added posts view, url and template.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def all_posts(request):
+    """ A view to show all blog posts """
+
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'posts/posts.html', context)
